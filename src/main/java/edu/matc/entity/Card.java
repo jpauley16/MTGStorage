a598719b62cdb99e0ab1825d9cd1c5ae08c39237
Jeff Pauley <jpauley@madisoncollege.edu>
2016-09-21 16:41:22 -0500

    readding project

---
+++ src/main/java/edu/matc/entity/Card.java
@@ -0,0 +1,7 @@
+package edu.matc.entity;
+
+/**
+ * Created by netherskub on 9/21/16.
+ */
+public class Card {
+}
---
+++ src/main/java/edu/matc/persistence/CardDao.java
@@ -0,0 +1,7 @@
+package edu.matc.persistence;
+
+/**
+ * Created by netherskub on 9/21/16.
+ */
+public class CardDao {
+}
