b86b856f462dd7533929162e0b436f1baa53f88e
Jeff Pauley <jpauley@madisoncollege.edu>
2016-09-20 22:54:20 -0500

    adding project directories

---
+++ src/main/java/com/MTGStorage/entity/Card.java
@@ -0,0 +1,7 @@
+package com.MTGStorage.entity;
+
+/**
+ * Created by netherskub on 9/20/16.
+ */
+public class Card {
+}
---
+++ src/main/java/com/MTGStorage/persistence/CardDao.java
@@ -0,0 +1,7 @@
+package com.MTGStorage.persistence;
+
+/**
+ * Created by netherskub on 9/20/16.
+ */
+public class CardDao {
+}
