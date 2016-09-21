e23756cb45d3424cb42b00fca8f6162045f839a0
Jeff Pauley <jpauley@madisoncollege.edu>
2016-09-21 15:26:33 -0500

    fixing stuff

--- src/main/java/com/MTGStorage/entity/Card.java
+++
@@ -1,7 +0,0 @@
-package com.MTGStorage.entity;
-
-/**
- * Created by netherskub on 9/20/16.
- */
-public class Card {
-}
--- src/main/java/com/MTGStorage/persistence/CardDao.java
+++
@@ -1,7 +0,0 @@
-package com.MTGStorage.persistence;
-
-/**
- * Created by netherskub on 9/20/16.
- */
-public class CardDao {
-}
